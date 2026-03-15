8cabd7866d1be87b26e4e715156b4e651896de43
SabrePenguin <lknofczynski@gmail.com>
2026-03-14 23:36:03 -0400

    Remove unnecessary proxies

--- src/main/java/me/modmuss50/svw/SimpleVoidWorld.java
+++ src/main/java/me/modmuss50/svw/SimpleVoidWorld.java
@@ -1,17 +1,12 @@
 package me.modmuss50.svw;
 
 import me.modmuss50.svw.blocks.SimpleVoidWorldBlocks;
-import me.modmuss50.svw.proxy.CommonProxy;
 import me.modmuss50.svw.world.VoidWorldProvider;
-import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.init.Blocks;
-import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.DimensionType;
 import net.minecraftforge.common.DimensionManager;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
@@ -20,8 +15,6 @@ public class SimpleVoidWorld {
 
 	public static VoidTab creativeTab;
 
-	@SidedProxy(clientSide = "me.modmuss50.svw.proxy.ClientProxy", serverSide = "me.modmuss50.svw.proxy.CommonProxy")
-	public static CommonProxy proxy;
 	public static DimensionType type;
 
 	@Mod.EventHandler
@@ -29,7 +22,6 @@ public class SimpleVoidWorld {
 		Config.load(event.getSuggestedConfigurationFile());
 
 		creativeTab = new VoidTab();
-		proxy.init();
 
 		type = DimensionType.register("simplevoidworld", "void", Config.dimID, VoidWorldProvider.class, false);
 		DimensionManager.registerDimension(Config.dimID, type);
