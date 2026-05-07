05303825e2c2256b0ba45026b90f352e2df6d298
SabrePenguin <lknofczynski@gmail.com>
2026-05-07 13:59:52 -0400

    Create the search pattern

--- src/main/java/me/modmuss50/svw/world/WorldTeleporter.java
+++ src/main/java/me/modmuss50/svw/world/WorldTeleporter.java
@@ -10,8 +10,9 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.Teleporter;
 import net.minecraft.world.WorldServer;
 
-import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
 
+@ParametersAreNonnullByDefault
 public class WorldTeleporter extends Teleporter {
 
 	BlockPos pos;
@@ -26,46 +27,53 @@ public class WorldTeleporter extends Teleporter {
 	@Override
 	public void placeInPortal(Entity entityIn, float rotationYaw) {
 		if (world.provider.getDimension() != Config.dimID && entityIn instanceof EntityPlayer) {
-			boolean foundBlock = false;
-			BlockPos.MutableBlockPos mutableBlockPos = new BlockPos.MutableBlockPos(pos.getX(), 0, pos.getZ());
-			for (int y = 0; y < 256; y++) {
-				mutableBlockPos.setY(y);
-				if (world.getBlockState(mutableBlockPos).getBlock() == SimpleVoidWorldBlocks.portal) {
-					pos = new BlockPos(pos.getX(), y + 1, pos.getZ());
-					foundBlock = true;
-					break;
+			BlockPos result = Config.portalAtY64 ? searchInRange(pos, 0, 256, Config.portalRadius) :
+					searchInRange(pos, Config.portalRadius);
+			if (result != null) {
+				pos = result.toImmutable();
+			} else {
+				boolean foundBlock = false;
+				BlockPos.MutableBlockPos mutableBlockPos = new BlockPos.MutableBlockPos(pos.getX(), 0, pos.getZ());
+				for (int y = 0; y < 256; y++) {
+					mutableBlockPos.setY(y);
+					if (world.getBlockState(mutableBlockPos).getBlock() == SimpleVoidWorldBlocks.portal) {
+						pos = new BlockPos(pos.getX(), y + 1, pos.getZ());
+						foundBlock = true;
+						break;
+					}
 				}
-			}
-			if(!foundBlock){
-				pos = ((EntityPlayer) entityIn).getBedLocation(world.provider.getDimension());
-				if(pos == null){
-					pos = world.provider.getRandomizedSpawnPoint();
+				if (!foundBlock) {
+					pos = ((EntityPlayer) entityIn).getBedLocation(world.provider.getDimension());
+					if (pos == null) {
+						pos = world.provider.getRandomizedSpawnPoint();
+					}
 				}
 			}
 
 		}
 		if (world.provider.getDimension() == Config.dimID) {
-			BlockPos result = nearestBlock();
+			BlockPos result = Config.portalAtY64 ? searchInRange(pos, 64, 8) : searchInRange(pos, 8);
 			if (result != null) {
-				pos = result;
-			}
-			//TODO look around for a free space so it doesnt place in a base?
-			pos = new BlockPos(pos.getX(), 64, pos.getZ());
-			if (world.getBlockState(pos).getBlock() != SimpleVoidWorldBlocks.portal) {
-				int color = world.rand.nextInt(15);
-				for (int x = -3; x < 4; x++) {
-					for (int z = -3; z < 4; z++) {
-						if (world.isAirBlock(pos.add(x, 0, z))) {
-							world.setBlockState(pos.add(x, 0, z), Blocks.STAINED_HARDENED_CLAY.getStateFromMeta(color));
-						}
+				pos = result.toImmutable();
+			} else {
+				//TODO look around for a free space so it doesnt place in a base?
+				int y = Config.portalAtY64 ? 64 : pos.getY();
+				pos = new BlockPos(pos.getX(), y, pos.getZ());
+				if (world.getBlockState(pos).getBlock() != SimpleVoidWorldBlocks.portal) {
+					int color = world.rand.nextInt(15);
+					for (int x = -3; x < 4; x++) {
+						for (int z = -3; z < 4; z++) {
+							if (world.isAirBlock(pos.add(x, 0, z))) {
+								world.setBlockState(pos.add(x, 0, z), Blocks.STAINED_HARDENED_CLAY.getStateFromMeta(color));
+							}
 
+						}
+					}
+					world.setBlockState(pos, SimpleVoidWorldBlocks.portal.getDefaultState());
+					for (EnumFacing facing : EnumFacing.HORIZONTALS) {
+						world.setBlockState(pos.up().offset(facing), Blocks.TORCH.getDefaultState());
 					}
 				}
-				world.setBlockState(pos, SimpleVoidWorldBlocks.portal.getDefaultState());
-				for(EnumFacing facing : EnumFacing.HORIZONTALS){
-					world.setBlockState(pos.up().offset(facing), Blocks.TORCH.getDefaultState());
-				}
-
 			}
 		}
 
@@ -77,34 +85,37 @@ public class WorldTeleporter extends Teleporter {
 	}
 
 	@SuppressWarnings("ConstantConditions")
-	@Nullable
-	private BlockPos nearestBlock() {
-		int radius = Config.portalRadius;
-		int originalX = pos.getX();
-		int originalZ = pos.getZ();
-		BlockPos.MutableBlockPos mutPos = new BlockPos.MutableBlockPos(originalX, 64, originalZ);
-		for (int y = 0; y <= radius; y++) {
-			for (int x = -radius; x <= radius; x++) {
-				for (int z = -radius; z <= radius; z++) {
-					mutPos.setPos(originalX + x, 64 + y, originalZ + z);
-					if (world.getBlockState(mutPos).getBlock() == SimpleVoidWorldBlocks.portal) {
-						mutPos.setY(mutPos.getY());
-						return mutPos;
+	private BlockPos.MutableBlockPos searchInRange(BlockPos original, int minY, int maxY, int radius) {
+		int originalX = original.getX();
+		int originalZ = original.getZ();
+		minY = Math.min(minY, 0);
+		maxY = Math.max(maxY, 255);
+		int newYCenter = (minY + maxY) / 2;
+		BlockPos.MutableBlockPos blockPos = new BlockPos.MutableBlockPos(original);
+		for (int x = -radius; x <= radius; x++) {
+			for (int z = -radius; z <= radius; z++) {
+				for (int y = newYCenter; y <= maxY; y++) {
+					blockPos.setPos(originalX + x, y, originalZ + z);
+					if (world.getBlockState(blockPos).getBlock() == SimpleVoidWorldBlocks.portal) {
+						return blockPos;
 					}
 				}
-			}
-		}
-		for (int y = -1; y >= -radius; y--) {
-			for (int x = -radius; x <= radius; x++) {
-				for (int z = -radius; z <= radius; z++) {
-					mutPos.setPos(originalX + x, 64 + y, originalZ + z);
-					if (world.getBlockState(mutPos).getBlock() == SimpleVoidWorldBlocks.portal) {
-						mutPos.setY(mutPos.getY());
-						return mutPos;
+				for (int y = newYCenter; y >= minY; y--) {
+					blockPos.setPos(originalX + x, y, originalZ + z);
+					if (world.getBlockState(blockPos).getBlock() == SimpleVoidWorldBlocks.portal) {
+						return blockPos;
 					}
 				}
 			}
 		}
 		return null;
+	}
+
+	private BlockPos.MutableBlockPos searchInRange(BlockPos original, int source, int radius) {
+		return searchInRange(original, source - radius, source + radius, radius);
+	}
+
+	private BlockPos.MutableBlockPos searchInRange(BlockPos original, int radius) {
+		return searchInRange(original, original.getY() - radius, original.getY() + radius, radius);
 	}
 }
